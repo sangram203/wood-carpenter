4a2a5b5195defea83e5212b1b149bcba6fc66f98
sangram203 <sangram.hati@gmail.com>
2019-07-23 18:12:21 +0530

    2 files modified, 28 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavComponent } from './includes/navbar/nav.component';
+import { FooterComponent } from './includes/footer/footer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ 
     AppComponent,
-    NavComponent 
+    NavComponent,
+    FooterComponent 
     ],
   bootstrap:    [ AppComponent ]
 })
