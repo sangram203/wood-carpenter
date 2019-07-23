327b8507d870002c75fffc82057ded5aa13b837f
sangram203 <sangram.hati@gmail.com>
2019-07-23 18:34:31 +0530

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavComponent } from './includes/navbar/nav.component';
 import { FooterComponent } from './includes/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'',redirectTo:'home',pathMatch:'full'},
+      {path:'home', component:HomeComponent},
+      {path:'login', component:LoginComponent}
+    ]) 
+    ],
   declarations: [ 
     AppComponent,
     NavComponent,
-    FooterComponent 
+    FooterComponent,
+    LoginComponent,
+    HomeComponent 
     ],
   bootstrap:    [ AppComponent ]
 })
