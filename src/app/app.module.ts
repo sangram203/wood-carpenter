203e05869c019beac21d7378fda690e6bfd69705
sangram203 <sangram.hati@gmail.com>
2019-07-23 17:24:57 +0530

    3 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+import { NavComponent } from './includes/navbar/nav.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ 
+    AppComponent,
+    NavComponent 
+    ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
